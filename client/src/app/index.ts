4b81263d0d1eb7d3f2161791e58a050f66e06f00
Joshua Gruber <JGruber@JGruber-mbp15r-3.local>
2016-06-09 12:27:12 -0600

    Using a new angular starter base

--- client/src/app/index.ts
+++ client/src/app/index.ts
@@ -1,31 +1,10 @@
-/// src/app/index.ts
-import {Component} from '@angular/core';
-import {RouteConfig, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
-import {FORM_PROVIDERS} from '@angular/common';
+// App
+export * from './app.component';
+export * from './app.service';
 
-import '../style/app.scss';
+import { AppState } from './app.service';
 
-import {Api} from './services/api';         // ./services/api/index.ts
-import routes from './routes';
-
-/*
- * App Component
- * Top Level Component
- */
-@Component({
-    selector: 'app', // <app></app>
-    providers: [...FORM_PROVIDERS, Api],
-    directives: [...ROUTER_DIRECTIVES],
-    pipes: [],
-    styles: [require('./style.scss')],
-    template: require('./template.html')
-})
-
-@RouteConfig(routes)
-
-export class App {
-    url: string = '//github.com/cmelion/generator-ng2-webpack';
-
-    constructor(public api: Api) {
-    }
-}
+// Application wide providers
+export const APP_PROVIDERS = [
+  AppState
+];
